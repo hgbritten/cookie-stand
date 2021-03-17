d13a4396548172a02688b6c036940a4dbd44024a
hunter britten <huntergbritten@gmail.com>
2021-03-16 17:50:11 -0700

    final test

--- app.js
+++ app.js
@@ -18,16 +18,8 @@ const timeSlots = [
     '7pm',
 
 ];
-// function createTable() {
+
 const profileContainer = document.getElementById('cookieTable');
-
-// const article = createChild('article', cookieTable);
-
-// createChild('h2', article, this.locaiton);
-
-// createChild('p', article, this.sales);
-
-// const ul = createChild('tr', article);
 
 const table = createChild('table', profileContainer);
 
@@ -42,25 +34,16 @@ for (let i = 0; i < timeSlots.length; i++) {
 createChild('th', headerRow, 'Daily Total');
 
 
-// createChild('th', sideRow, 'Seattle');
-// createChild('td', th);
-// const tr = createChild('tr', table);
-// const th = createChild('th', tr);
-// const tbody = createChild('td', sideRow);
-
-// createChild('td', dataRow, this.location);
-// createChild('th', dataRow, this.sales);
-// }
-
 function CookieRow(location, min, max, avgcookies) {
     this.location = location;
     this.sales = sales(min, max, avgcookies);
+    console.log(this.sales);
     // this.int = getRandomInt(23, 65, 6.3);
-
+    CookieRow.stores.push(this);
     // const dataRow = createChild('tr', table);
 }
 
-
+CookieRow.stores = [];
 
 CookieRow.prototype.render = function () {
     const profileContainer = document.getElementById('cookieTable');
@@ -79,6 +62,25 @@ CookieRow.prototype.render = function () {
     let currentSales = total
     createChild('td', sideRow, currentSales);
 }
+
+
+CookieRow.prototype.renderfooter = function () {
+    const tr = createChild('tr', table, 'Hourly Total')
+    let total = 0
+    for (let i = 0; i < timeSlots.length; i++) {
+        let timeTotal = 0
+        for (let j = 0; j < CookieRow.stores.length; j++) {
+            timeTotal += CookieRow.stores[j].sales[i];
+
+        }
+        total += timeTotal;
+        console.log(timeTotal);
+        createChild('td', tr, timeTotal);
+    }
+    createChild('td', tr, total);
+
+}
+
 function createChild(tag, parent, text) {
     const child = document.createElement(tag);
     parent.appendChild(child);
@@ -91,35 +93,15 @@ function createChild(tag, parent, text) {
 
 }
 
-// for (let i = 0; i < timeSlots.length; i += 1) {
-//     const currentSales = cookieStand.sales[i];
-//     const currentTimeSlot = timeSlots[i];
-//     hourItemElem.textContent = currentTimeSlot + ': ' + currentSales + ' cookies';
-//     let total = 0;
-//     for (let i = 0; i < cookieStand.sales.length; i += 1) {
-//         const currentSales = cookieStand.sales[i];
-//         total += currentSales;
-//     }
-// }
-
 let seattle = new CookieRow('Seattle', 23, 65, 6.3)
 
 let dubai = new CookieRow('Dubai', 11, 38, 3.7)
 
 let tokyo = new CookieRow('Tokyo', 3, 24, 1.2)
-// location: 'Tokyo',
-// sales: sales(3, 24, 1.2),
-
 
 let paris = new CookieRow('Paris', 20, 38, 2.3)
-//     location: 'Paris',
-//     sales: sales(20, 38, 2.3),
-// }
 
 let lima = new CookieRow('Lima', 2, 16, 4.6)
-// location: 'Lima',
-// sales: sales(2, 16, 4.6),
-
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -128,10 +110,6 @@ function getRandomInt(min, max) {
 }
 
 console.log(getRandomInt)
-
-// Create a function that creates a new array
-// And pushes random ints into every element of the array
-
 
 
 function sales(min, max, avgcookies) {
@@ -146,11 +124,6 @@ function sales(min, max, avgcookies) {
     return arraySales;
 }
 
-// arrpush(getRandomInt(23, 65));
-// let randomInt = getRandomInt(23, 25);
-// arrpush(randomInt);
-
-let cookieStand = seattle;
 
 const storeContainerElem = document.getElementById('store-container')
 
@@ -163,57 +136,5 @@ dubai.render();
 paris.render();
 lima.render();
 
+seattle.renderfooter();
 
-// renderCookieStand(seattle);
-// renderCookieStand(tokyo);
-// renderCookieStand(dubai);
-// renderCookieStand(paris);
-// renderCookieStand(lima);
-
-// function renderCookieStand(cookieStand) {
-
-//     const sectionElem = document.createElement('section');
-//     storeContainerElem.appendChild(sectionElem);
-
-//     const locationElem = document.createElement('h2');
-//     sectionElem.appendChild(locationElem);
-//     locationElem.textContent = cookieStand.location;
-
-//     const hourListElem = document.createElement('ul');
-//     sectionElem.appendChild(hourListElem);
-
-//     for (let i = 0; i < timeSlots.length; i += 1) {
-
-//         // need a bunch of lis in the ul
-//         // lets do one for now
-//         const hourItemElem = document.createElement('li');
-//         hourListElem.appendChild(hourItemElem);
-
-//         const currentSales = cookieStand.hourlySales[i];
-//         const currentTimeSlot = timeSlots[i];
-//         hourItemElem.textContent = currentTimeSlot + ': ' + currentSales + ' cookies';
-//     }
-
-//     
-
-
-
-//     }
-//     // the sum of all the sales for the current cookieStand
-
-//     // need a bunch of lis in the ul
-//     // lets do one for now
-
-//     const hourItemElem = document.createElement('li');
-//     hourListElem.appendChild(hourItemElem);
-
-//     hourItemElem.textContent = seattle.sixAm;
-
-//     // need last total item
-
-//     const totalItemElem = document.createElement('li');
-//     hourListElem.appendChild(totalItemElem);
-//     totalItemElem.textContent = 'Total: ' + total + ' cookies';
-
-
-// 
